78991f929743099c4432cd93df2324473fd66a3c
Benen66 <benling@umich.edu>
2022-04-05 10:23:57 -0400

    weight

--- web-form.py
+++ web-form.py
@@ -27,7 +27,7 @@ with st.form("graphForm", clear_on_submit=False):
        'Party Cmte', 'Candidate', 'Non-contribution'
     ), index=0)
     keywords = st.text_input("Type Keywords Here")
-    dollarThreshold = st.slider("$ Threshold for Relevance", 0, 1000000, 10000)
+    dollarThreshold = st.slider("$ Threshold for Relevance", 0, 100000, 10000)
     commonBills = st.number_input('Number of bills in common', value=1)
     submit = st.form_submit_button("Submit")
 
@@ -54,7 +54,7 @@ if industryOrTopic == 'None':
 else:
    # Code for filtering dataframe and generating network
     lob_df = filter_spending(sector=industryOrTopic, keywords=keywords, date_min=dateRange[0], date_max=dateRange[1])
-    graph_df = format_graph_data(lob_df, minAmount=dollarThreshold)
+    graph_df = format_graph_data(lob_df, minAmount=dollarThreshold, commonBills=commonBills)
     # st.write(graph_df.to_string())
     lobby_net = Network(height='750px', width='100%', bgcolor='white', font_color='black')
     G = nx.from_pandas_edgelist(graph_df, 'source', 'dest', 'weight')
@@ -79,6 +79,8 @@ else:
     components.html(HtmlFile.read(), height=435)
 
 # to do:
+# how to host this online
 # loading widget while graph is being generated?
 # show message if nothing in graph?
+# fix number of bills in common filter => make sure it works
 
